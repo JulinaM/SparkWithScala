5242583cfc20507c731cc55eb87d2eee64fdb348
jmaharjan <jmaharjan@deerwalk.com>
2017-01-06 15:59:16 +0545

    testssss

--- src/main/scala/com/julina/scala/Tutorial/HelloWorld.scala
+++ src/main/scala/com/julina/scala/Tutorial/HelloWorld.scala
@@ -8,7 +8,14 @@ object HelloWorld {
   def main(args: Array[String]) : Unit ={
       println("Hello World")
     val name = "Julina"
-    val nameHasUpperCase = name.exists(_.isUpper)
+    val nameHasUpperCase = name.exists(_.isUpper) // this is called predicates
     println(nameHasUpperCase)
+
+    println("i dont know why this is not working")
+    println("i dont know why this is not working")
+    println("i dont know why this is not working")
+    ///
+
+
   }
 }
